dfbbf255bdd1528a8b46437231676f55101db583
LostPy <lostmusic.osu@gmail.com>
2021-05-02 00:15:25 +0200

    1.0.1
    Add setup and update readme

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+import pyDocStr
+
+__doc__ = """A package and a parser to document pythons files quickly."""
+
+
+setup(
+	name='pyDocStr',
+	version='1.0',
+	author='LostPy',
+	description="A package to document pythons files.",
+	long_description=__doc__,
+    package_dir = {'pyDocStr': './pyDocStr'},
+    package_data = {'': ['LICENSE.txt', 'requirements.txt']},
+	include_package_data=True,
+	url='https://github.com/LostPy/pydocstr',
+	classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: Functionnal",
+        "License :: MIT",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5+"
+    ],
+    license='MIT',
+    packages = find_packages()
+    )
